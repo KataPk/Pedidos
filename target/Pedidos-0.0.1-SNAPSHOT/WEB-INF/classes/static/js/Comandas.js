f5a9cc033622e58fea79771dab15f196e9038c00
KataPk <carlos.silva1245@outlook.com.br>
2023-10-06 19:35:58 -0300

    Run test

--- target/Pedidos-0.0.1-SNAPSHOT/WEB-INF/classes/static/js/Comandas.js
+++
@@ -1,8 +0,0 @@
-const removeButtons = document.querySelectorAll('.remove');
-
-removeButtons.forEach((button) => {
-    button.addEventListener('click', (event) => {
-        const order = event.target.closest('.order');
-        order.remove();
-    });
-});
