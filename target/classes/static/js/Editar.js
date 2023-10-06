b200cb0ab4c06400a286536a43dad2edac414957
KataPk <carlos.silva1245@outlook.com.br>
2023-10-06 19:43:11 -0300

    jar

---
+++ target/classes/static/js/Editar.js
@@ -0,0 +1,72 @@
+const lessButtons = document.querySelectorAll('.less');
+const plusButtons = document.querySelectorAll('.plus');
+const quantElements = document.querySelectorAll('.quant');
+const precoElements = document.querySelectorAll('.preco');
+const idElements = document.querySelectorAll('.id');
+const totalElements = document.querySelectorAll('.total');
+const removeButtons = document.querySelectorAll('.remover');
+let valorTotal = document.querySelector('#total');
+
+let arrayValue = document.querySelector('#itemArray')
+
+let tempTotal = 0;
+
+for (let i = 0; i < plusButtons.length; i++) {
+    const price = parseFloat(precoElements[i].getAttribute('data-price'));
+    let quantData = parseInt(quantElements[i].getAttribute('data-quant'));
+    const pedidoId = parseInt(idElements[i].getAttribute('data-id'));
+
+    quantElements[i].innerHTML = quantData.toString()
+    let total = (price * quantData).toFixed(2).toString().replace('.', ',')
+
+    totalElements[i].innerHTML = `Total: R$${total}`;
+
+    plusButtons[i].addEventListener('click', function () {
+        let quant = parseInt(quantElements[i].innerHTML);
+        quantData++;
+        quant = quantData
+        quantElements[i].innerHTML = quant.toString();
+        let total = (price * quant).toFixed(2).toString().replace('.', ',');
+        totalElements[i].innerHTML = `Total: R$${total}`;
+        tempTotal += price
+        valorTotal.innerHTML = `Total: R$${(tempTotal).toFixed(2).replace('.', ',')}`
+
+        const url
+
+    });
+
+    lessButtons[i].addEventListener('click', function () {
+        let quant = parseInt(quantElements[i].innerHTML);
+        if (quant > 0) {
+            quantData--;
+            quant = quantData
+            quantElements[i].innerHTML = quant.toString();
+            let total = (price * quant).toFixed(2).toString().replace('.', ',');
+            totalElements[i].innerHTML = `Total: R$${total}`;
+        }
+        tempTotal -= price
+        valorTotal.innerHTML = `Total: R$${(tempTotal).toFixed(2).replace('.', ',')}`
+    });
+
+    removeButtons[i].addEventListener('click', function () {
+        const cardProduto = this.parentNode.parentNode.parentNode
+        cardProduto.remove();
+        tempTotal -= price * quantData;
+        if (quantElements.length === 0) {
+            tempTotal = 0;
+            valorTotal.innerHTML = `Total: R$0,00`;
+        } else {
+            valorTotal.innerHTML = `Total: R$${(tempTotal).toFixed(2).replace('.', ',')}`;
+        }
+    });
+
+    valorTotal.innerHTML = `R$${(price * quantData).toFixed(2).replace('.', ',')}`
+    tempTotal += price * quantData
+}
+
+if (quantElements.length === 0) {
+    tempTotal = 0;
+    valorTotal.innerHTML = `Total: R$0,00`;
+} else {
+    valorTotal.innerHTML = `Total: R$${tempTotal.toFixed(2).replace('.', ',')}`;
+}
---
+++ target/classes/static/js/mesas.js
@@ -0,0 +1,70 @@
+
+{
+
+    // Tentar depois
+
+    // $(document).ready(function () {
+    //     function atualizarMesas() {
+    //         console.log("Atualizando...")
+    //         const mesasContainer = document.getElementById('mesas-container');
+    //         if (mesasContainer) {
+    //             $.ajax({
+    //                 type: 'GET',
+    //                 url: '/api/user/mesas/atualizar',
+    //                 success: function (data) {
+    //                     // Verifique novamente se o elemento ainda existe antes de atualizá-lo
+    //                     const updatedContainer = document.getElementById('mesas-container');
+    //                     if (updatedContainer) {
+    //                         updatedContainer.innerHTML = '';
+    //                         data.mesas.forEach(mesa => {
+    //                             const mesaElement = document.createElement('div');
+    //                             mesaElement.textContent = `Mesa ${mesa.numMesa}`;
+    //                             updatedContainer.appendChild(mesaElement);
+    //                         });
+    //                         atualizarModaisMesas(data);
+    //                     }
+    //                 },
+    //                 error: function (error) {
+    //                     console.log('Erro ao atualizar as mesas:', error.responseText);
+    //                 }
+    //             });
+    //         }
+    //     }
+    //
+    //     // Chame a função de atualização de mesas a cada 1 minuto (60.000 milissegundos)
+    //     setInterval(atualizarMesas, 20000);
+    //
+    //     // Função para atualizar o conteúdo dos modais de mesas com os dados atualizados
+    //     function atualizarModaisMesas(mesas) {
+    //         mesas.forEach(function (mesa) {
+    //             var modal = $('#ModalMesa' + mesa.numMesa);
+    //             if (modal.length) {
+    //                 modal.find('.modal-title').text('Mesa ' + mesa.numMesa);
+    //                 modal.find('.clientNameMesa').val('');
+    //                 modal.find('.mesaId').val(mesa.id);
+    //             }
+    //         });
+    //     }
+    // });
+
+
+
+    // ADM
+}
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
